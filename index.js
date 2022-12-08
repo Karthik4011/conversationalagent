6221fbdc147686c8522e39f2932d6d9eb28b9a97
Karthik4011 <dattu.sai4@gmail.com>
2022-12-08 14:19:10 -0500

    fixed '/' method

--- index.js
+++ index.js
@@ -99,6 +99,6 @@ getCountyDetails = (req,res)=>{
 
   res.status(200).send(jsonResponse);
 }
-app.get('/',"Hello")
+app.get('/',res.status(200).send("sfds"))
 
 app.get('/api/getCountyDetails',getCountyDetails)
